6c28f8366d308a9775852a24f5a6a8632c350734
Gilles Lepretre <glepret.pro@gmail.com>
2021-05-05 18:09:19 +0200

    Update span left/right to 1920x1080
    Even though natively supported by Ubuntu MATE

--- span-left.sh
+++ span-left.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 current_x=$(xwininfo -id "$(xdotool getactivewindow)" | grep "Absolute upper-left X" | awk '{print $NF}')
 
-if [ "$current_x" -ge 1680 ]
+if [ "$current_x" -ge 1920 ]
 then
-  screen_offset=1680
+  screen_offset=1920
 else
   screen_offset=0
 fi
-wmctrl -r :ACTIVE: -e 0,$screen_offset,0,840,1050
+wmctrl -r :ACTIVE: -e 0,$screen_offset,0,960,1080
